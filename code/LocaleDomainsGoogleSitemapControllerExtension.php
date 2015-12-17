92e2483627b38fb63a069af7f96b6042c8e4f2a0
helpfulrobot <chris@silverstripe.com>
2015-12-18 11:26:34 +1300

    Converted to PSR-2

--- code/LocaleDomainsGoogleSitemapControllerExtension.php
+++ code/LocaleDomainsGoogleSitemapControllerExtension.php
@@ -1,13 +1,16 @@
 <?php
-class LocaleDomainsGoogleSitemapControllerExtension extends Extension {
-	
-	public function onBeforeInit() {
-		Translatable::set_current_locale(LocaleDomains::getLocaleFromHost());
-	}
-	
-	public function updateGoogleSitemapItems(ArrayList $items, $class, $page) {
-		if($class == "SiteTree") {
-			$items = $items->filter("Locale", LocaleDomains::getLocaleFromHost());
-		}
-	}
+class LocaleDomainsGoogleSitemapControllerExtension extends Extension
+{
+    
+    public function onBeforeInit()
+    {
+        Translatable::set_current_locale(LocaleDomains::getLocaleFromHost());
+    }
+    
+    public function updateGoogleSitemapItems(ArrayList $items, $class, $page)
+    {
+        if ($class == "SiteTree") {
+            $items = $items->filter("Locale", LocaleDomains::getLocaleFromHost());
+        }
+    }
 }
